b98486d069f7d1a6824c026afe8065fceeba2b99
Graeme Williams <graeme.m.williams@gmail.com>
2024-12-23 16:27:10 -0500

    Add user-input defined circle position

--- AxeGame/main.cpp
+++ AxeGame/main.cpp
@@ -1,14 +1,17 @@
 #include "raylib.h"
 #include <string>
+#include "main.h"
 
 int main(int argc, char const *argv[])
 {
-    // Tunables
+    // ***Tunables***
+    // Window + Game Settings
     int gameWidth = 800;
     int gameHeight = 600;
     std::string gameTitle = "Axe Game";
     int targetFPS = 60;
 
+    // Characters
     int circleRadius = 100;
     Color circleColor = BLUE;
     float circleMoveSpeed = 1000.0;
@@ -17,7 +20,7 @@ int main(int argc, char const *argv[])
     int windowMidCol = round(gameWidth / 2);
     int windowMidRow = round(gameHeight / 2);
 
-    // Execution
+    // ***Execution***
     // Init Window/Settings
     InitWindow(gameWidth, gameHeight, gameTitle.c_str());
     SetTargetFPS(targetFPS);
@@ -27,9 +30,8 @@ int main(int argc, char const *argv[])
     int circleCol = windowMidCol;
     while (!WindowShouldClose())
     {
-        // Traverse to right, reset to left
-        if (circleCol < (gameWidth+circleRadius)) { circleCol += (int)round(GetFrameTime() * circleMoveSpeed); }
-        else { circleCol = -circleRadius; }
+        GetUserInputCircleCol(circleMoveSpeed, circleCol, gameWidth, circleRadius);
+        GetUserInputCircleRow(circleMoveSpeed, circleRow, gameHeight, circleRadius);
 
         BeginDrawing();
         ClearBackground(WHITE);
@@ -40,3 +42,26 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+void GetUserInputCircleCol(float circleMoveSpeed, int &circleCol, int gameWidth, int circleRadius)
+{
+    int colOffset = 0;
+    if (IsKeyDown(KEY_RIGHT)) { colOffset += (int)round(GetFrameTime() * circleMoveSpeed); }
+    if (IsKeyDown(KEY_LEFT)) { colOffset -= (int)round(GetFrameTime() * circleMoveSpeed); }
+    circleCol += colOffset;
+
+    // Check for edges, loop around
+    if (circleCol > gameWidth + circleRadius) { circleCol = -circleRadius; }
+    else if (circleCol < -circleRadius) { circleCol = gameWidth + circleRadius; }
+}
+
+void GetUserInputCircleRow(float circleMoveSpeed, int &circleRow, int gameHeight, int circleRadius)
+{
+    int rowOffset = 0;
+    if (IsKeyDown(KEY_DOWN)) { rowOffset += (int)round(GetFrameTime() * circleMoveSpeed); }
+    if (IsKeyDown(KEY_UP)) { rowOffset -= (int)round(GetFrameTime() * circleMoveSpeed); }
+    circleRow += rowOffset;
+
+    // Check for edges, loop around
+    if (circleRow > gameHeight + circleRadius) { circleRow = -circleRadius; }
+    else if (circleRow < -circleRadius) { circleRow = gameHeight + circleRadius; }
+}
---
+++ AxeGame/main.h
@@ -0,0 +1,3 @@
+#pragma once
+void GetUserInputCircleCol(float circleMoveSpeed, int &circleCol, int gameWidth, int circleRadius);
+void GetUserInputCircleRow(float circleMoveSpeed, int &circleRow, int gameHeight, int circleRadius);
