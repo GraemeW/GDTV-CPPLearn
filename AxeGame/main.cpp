69e9013e183575d14954ed6b655d9c007943c0be
Graeme Williams <graeme.m.williams@gmail.com>
2024-12-23 16:07:25 -0500

    Draw circle to screen

--- AxeGame/main.cpp
+++ AxeGame/main.cpp
@@ -7,18 +7,33 @@ int main(int argc, char const *argv[])
     int gameWidth = 800;
     int gameHeight = 600;
     std::string gameTitle = "Axe Game";
+    int targetFPS = 60;
 
-    // Misc Game Input
-    float rootBeer{1.99};
-    double cheeseBurger{5.99};
-    bool shouldHaveLunch{true};
+    int circleRadius = 100;
+    Color circleColor = BLUE;
+    float circleMoveSpeed = 1000.0;
+
+    // Derived Parameters
+    int windowMidCol = round(gameWidth / 2);
+    int windowMidRow = round(gameHeight / 2);
 
     // Execution
+    // Init Window/Settings
     InitWindow(gameWidth, gameHeight, gameTitle.c_str());
-    while (true)
+    SetTargetFPS(targetFPS);
+
+    // Circle start point 
+    int circleRow = windowMidRow;
+    int circleCol = windowMidCol;
+    while (!WindowShouldClose())
     {
+        // Traverse to right, reset to left
+        if (circleCol < (gameWidth+circleRadius)) { circleCol += (int)round(GetFrameTime() * circleMoveSpeed); }
+        else { circleCol = -circleRadius; }
+
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
+        DrawCircle(circleCol, circleRow, circleRadius, circleColor);
         EndDrawing();
     }
 
