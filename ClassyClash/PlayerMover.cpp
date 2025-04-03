07964a2892428b860454bec715b09fe27d8606cc
Graeme Williams <graeme.m.williams@gmail.com>
2025-04-03 12:46:48 -0400

    Fix player movement normalization

--- ClassyClash/PlayerMover.cpp
+++ ClassyClash/PlayerMover.cpp
@@ -12,27 +12,15 @@ Vector2 PlayerMover::GetPositionShift() {
     bool hasMoved = false;
 
     // L/R
-    if (IsKeyDown(KEY_A)) {
-        positionShift = Vector2Add(positionShift, Vector2{speed * frameTime, 0.0});
-        hasMoved = true;
-    }
-    if (IsKeyDown(KEY_D)) {
-        positionShift = Vector2Subtract(positionShift, Vector2{speed * frameTime, 0.0});
-        hasMoved = true;
-    }
+    if (IsKeyDown(KEY_A)) { positionShift = Vector2Add(positionShift, Vector2{1.0, 0.0}); hasMoved = true; }
+    if (IsKeyDown(KEY_D)) { positionShift = Vector2Subtract(positionShift, Vector2{1.0, 0.0}); hasMoved = true; }
 
     // U/D
-    if (IsKeyDown(KEY_W)) {
-        positionShift = Vector2Add(positionShift, Vector2{0.0, speed * frameTime});
-        hasMoved = true;
-    }
-    if (IsKeyDown(KEY_S)) {
-        positionShift = Vector2Subtract(positionShift, Vector2{0.0, speed * frameTime});
-        hasMoved = true;
-    }
+    if (IsKeyDown(KEY_W)) { positionShift = Vector2Add(positionShift, Vector2{0.0, 1.0}); hasMoved = true; }
+    if (IsKeyDown(KEY_S)) { positionShift = Vector2Subtract(positionShift, Vector2{0.0, 1.0}); hasMoved = true; }
 
     isMoving = hasMoved;
-    return positionShift;
+    return Vector2Scale(Vector2Normalize(positionShift), speed * frameTime);
 }
 
 bool PlayerMover::IsMoving() { return isMoving; }
